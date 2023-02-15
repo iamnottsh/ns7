c5a5fc788b2a6a246bd2663edda2060d19931dfa
ghgcx <iamnottsh@163.com>
2023-02-15 15:50:25 +0800

    创建标题

--- src/Site.jsx
+++ src/Site.jsx
@@ -1,5 +1,6 @@
 import {useEffect, useState} from 'react'
 import ns4 from "./ns4.js";
+import {Box, Container, Link, Typography} from "@mui/material";
 
 function Site() {
     const [module, setModule] = useState()
@@ -8,9 +9,15 @@ function Site() {
     }, [])
 
     return (
-        <p>
-            {module?.encode('23')}
-        </p>
+        <Container component="main" sx={{mt: 2, mb: 2}}>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <Typography variant="h3" sx={{flexGrow: 1}}>喵喵隐者4</Typography>
+                <Link href="https://gxlt.lanzouf.com/il0gr0nlq4bg" target="_blank" rel="noopener">安卓版</Link>
+            </Box>
+            <p>
+                {module?.encode('23')}
+            </p>
+        </Container>
     )
 }
 
